fe8b2a4373a2a08be28b3b263064fc10a664cb8b
Jamie Gorson <jamie.gorson@students.olin.edu>
2014-11-15 20:58:43 -0500

    HACKATHON

--- structure/structure.ino
+++ structure/structure.ino
@@ -1,17 +1,37 @@
-
+#include <iostream>     // std::cout, std::ostream, std::hex
+#include <sstream>      // std::stringbuf
+#include <string>       // std::string
 
 //Hot Rod Lincoln
-char hrl[] = "-----2-----.-----.-----.-----.-----.-----.-----.-----0-----0-----4-----4----2-----2-----4-----2-----5-----2-----4-----5-----4-----2------3-----4----0-----0-----4-----4----2-----2-----4-----2-----5-----2-----4-----5-----4-----2------3-----4-----2-----2----1-----1-----4-----4----1------4----2------4----1-----2-----1------4-----0-----1-----2-----2------4-----2----2-----2------4-----2----2------4-----2----2-----.-----.--001220......----20----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..\0";
-int hrlTime = 200
+char hrl[] = "-----2-----.-----.-----.-----.-----.-----.-----.-----0-----0-----4-----4----2-----2-----4-----2-----5-----2-----4-----5-----4-----2------3-----4----0-----0-----4-----4----2-----2-----4-----2-----5-----2-----4-----5-----4-----2------3-----4-----2-----2----1-----1-----4-----4----1------4----2------4----1-----2-----1------4-----0-----1-----2-----2------4-----2----2-----2------4-----2----2------4-----2----2-----.-----.--001220......----20----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..----..";
+int hrlTime = 200;
 
-const char *song_table[]=
+//Tamacun
+//char* tamacun = new char[3000];
+
+char tamacun[]= "....1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...0..2-1...-1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...-0..3-0...1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...0..2-1...-1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3.2.-0.-1.3...-0..1-.22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...0..2-1...-1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...-0..3-0...1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1...0..2-1...-1..22--2.-1...0..00--0.-0...3...1..02--0.-1...3...1.02-1..2-..-......0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1...-.....0..1-.2-..-0...1...3...-0...-.0...-.3...1...0..3-..1..2-..-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1..2-..-2...-1...-0..3-..1...0...-3.0.-.-.1.0.-.-.0.0.-.-3.31-..2...-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1...-.....0..1-.2-..-0...1...3...-0...-.0...-.3...1...0..3-..1..2-..-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1..2-..-2...-1...-0..3-..1...0...-3.0.-.-.1.0.-.-.0.0.-.-3.31-..2...-..20201.--20---...3.3.0.-.3.-.-...20201.--20---...3.3.0.-.3.-.-...20201.--20---...3.3.0.-.3.-.-...20201.--20---....10100.--10---..20201.--20---...3.3.0.-.3.-.-...20201.--20---...3.3.0.-.3.-.-...20201.--20---...3.3.0.-.3.-.-...20201.--20---10101010100.....2--.........0.............3.-.........-2...............-2...........0.............3.-.........-2...............-2...........0.............3.-.........-2...............-2...........0.............3.-.........-2...............3.-.0.3.-0.1.1...1...0...0.0..3-3.3...1.1.1...0.1.3.1.02-13-2.0.2.-.0...1.3...1.132--0.......0.4.-2.-0.2.3.-.0.3.1.02-1.02-03--..0.3...3.3...1.1.1.-0...0.0..3-3.3.-1...1.1...0.0.0.4...3.1...3.010...0.1.3.1.0.1.0..3--0.1.03--03-..1.3.-03-1.3.0.......-.......123--2.-1.3.1...123--2.-1.-01-..123--2.-1.3.2.-.3.-02--.1.3.1...012--1.-0.1.0...012--1.-0.3.0...012--1.-0.1.2.-.1.3.2.-.0.1.0..23-0.3.-2.-02-..23-0.3.-2.-12-..23-0.3.-2.-020--.0.1.0.-.1.3.2.-1.2.12--2.-022--.0.1.2.-.1.3.2.-..0000000011111111333333334.......-...0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1...-.....0..1-.2-..-0...1...3...-0...-.0...-.3...1...0..3-..1..2-..-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1..2-..-2...-1...-0..3-..1...0...-3.0.-.-.1.0.-.-.0.0.-.-3.31-..2...-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1...-.....0..1-.2-..-0...1...3...-0...-.0...-.3...1...0..3-..1..2-..-.....0..1-.2-..-0...1...3...1...-.2...-2...-1...0.1.0..2-..1..2-..-2...-1...-0..3-..1...0...-3.0.-.-.1.0.-.-.0.0.-.-3.31-..2...2.------";
+
+int tamacunTime = 60;
+
+const int MAX= 10;
+ char *song_table[MAX]=
 {
   hrl,
+  tamacun,
+};
+
+int timer_table[MAX]=
+{
+  //200,
   hrlTime,
-}
+  tamacunTime,
+};
 
-
-
+char actuators[] = "000000";
+int pins[6]= {};
+char note;
+int timer;
+char * notesP;
 //Pin connected to ST_CP of 74HC595
 int latchPin = 7;
 //Pin connected to SH_CP of 74HC595
@@ -20,6 +40,9 @@ int clockPin = 13;
 int dataPin = 11;
 
 int masterResetPin = 8;
+
+int rhInPin0;
+int rhOutPin0;
 int rhInPin1;
 int rhOutPin1;
 int rhInPin2;
@@ -30,12 +53,11 @@ int rhInPin4;
 int rhOutPin4;
 int rhInPin5;
 int rhOutPin5;
-int rhInPin6;
-int rhOutPin6;
 
 
 
-long time_start = 0
+
+long time_start = 0;
 unsigned long leftHand = 0;
 unsigned int rh1; unsigned int rh2; unsigned int rh3; unsigned int rh4; unsigned int rh5; unsigned int rh6;
 
@@ -45,39 +67,76 @@ void setup() {
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(masterResetPin, OUTPUT);
+  
+  pinMode(rhInPin0, OUTPUT);
+  pinMode(rhOutPin0, OUTPUT);
+  pinMode(rhInPin1, OUTPUT);
+  pinMode(rhOutPin1, OUTPUT);
+  pinMode(rhInPin2, OUTPUT);
+  pinMode(rhOutPin2, OUTPUT);
+  pinMode(rhInPin3, OUTPUT);
+  pinMode(rhOutPin3, OUTPUT);
+  pinMode(rhInPin4, OUTPUT);
+  pinMode(rhOutPin4, OUTPUT);
+  pinMode(rhInPin5, OUTPUT);
+  pinMode(rhOutPin5, OUTPUT);
+  
 }
 
+
+  bool input = true;
 void loop()
 {
 //USERINPUT to assign notes
   //static note =silent;
+
+  char note;
+  long int time;
   if (input){
-  notes = (char*)(&(song_table[i]));
-  timer = (int*)(&(song_table[i+1]));
+    int i;
+    i=0;
+   notesP = song_table[i];
+  time = timer_table[i];
+  input = false;
+  
+  // SET ACTUATORS TO ALL 0
+  //Serial.println("hi");
   }
-  if(timetodostuff(timer) && notes !="\0")
+
+  note = *notesP;
+ 
+  if(timetodostuff(time))// && note !='\0')
   {
-    playnote();
+    playNote();
     time_start = millis();
-    getnextnote(leftHand, rh1, rh2, rh3, rh4, rh5, rh6);
-    
-  }
+    getnextnote(leftHand);
+   
+   }
 }    
 
 
-
-bool timetodostuff(timer){
-  if millis() - time_start < timer{return false}
-  else{return true}
+bool timetodostuff(int timer){
+  if (millis() - time_start < timer){return false;}
+  else{return true;}
 }
 
-bool getnextnote(unsigned long & leftHand, unsigned int & rh1, unsigned int & rh2, unsigned int & rh3, unsigned int & rh4, unsigned int & rh5, unsigned int & rh6){
-  value1 = notes[i]
-  value2 = notes[i+1]
-  value3 = notes[i+2]
-  value4 = notes[i+3]
-  value5 = notes[i+4]
-  value6 = notes[i+5]
+bool getnextnote(unsigned long &leftHand){//, unsigned int &rh1, unsigned int &rh2, unsigned int &rh3, unsigned int &rh4, unsigned int &rh5, unsigned int &rh6){
+  char value1; char value2; char value3; char value4; char value5; char value6;
+  unsigned int outs1; unsigned int outs2; unsigned int outs3; unsigned int outs4; unsigned int outs5; unsigned int outs6; unsigned int out1; unsigned int out2; unsigned int out3; unsigned int out4; unsigned int out5; unsigned int out6;
+
+  value1 = *notesP;
+  notesP = notesP+1;
+  value2 = *notesP;
+  notesP = notesP+1;
+  value3 = *notesP;
+  notesP = notesP+1;
+  value4 = *notesP;
+  notesP = notesP+1;
+  value5 = *notesP;
+  notesP = notesP+1;
+  value6 = *notesP;
+  notesP = notesP+1;
+
   
   out6 = pushdownfret(value6, outs6);
       outs6 = out6;
@@ -93,18 +152,20 @@ bool getnextnote(unsigned long & leftHand, unsigned int & rh1, unsigned int & rh
       outs1 = out1;
   leftHand =(out6 << 25) + (out5<<20) + (out4<<15) + (out3<<10) + (out2<<5) + out1;
   
-  state6 = strum(value6, states6, strum6Right, strum6Left);       
-  state5 = strum(value5, states5, strum5Right, strum5Left);         
-  state4 = strum(value4, states4, strum4Right, strum4Left);
-  state3 = strum(value3, states3, strum3Right, strum3Left);
-  state2 = strum(value2, states2, strum2Right, strum2Left);
-  state1 = strum(value1, states1, strum1Right, strum1Left);
-  
-  
-  return True
+  strum(value6, 5, rhInPin5, rhOutPin5);       
+  strum(value5, 4, rhInPin4, rhOutPin4);         
+  strum(value4, 3, rhInPin3, rhOutPin3);
+  strum(value3, 2, rhInPin2, rhOutPin2);
+  strum(value2, 1, rhInPin1, rhOutPin1);
+  strum(value1, 0, rhInPin0, rhOutPin0);
+
+  return true;
  
 }
 bool playNote(){
+   unsigned long shiftreg1; unsigned long shiftreg2; unsigned long shiftreg3; unsigned long shiftreg4; unsigned long shiftreg5; unsigned long shiftreg6;
+  
+  
     shiftreg1 = leftHand >> 24;
     shiftreg2 = (leftHand - (shiftreg1<<24)) >>16;
     shiftreg3 = (leftHand - (shiftreg1<<24) - (shiftreg2<<16)) >>8;
@@ -119,71 +180,65 @@ bool playNote(){
     shiftOut(dataPin, clockPin, LSBFIRST, shiftreg1);
     
     digitalWrite(latchPin,HIGH); //Prepares latch
-      
-      digitalWrite(state6, HIGH)
-      digitalWrite(state5, HIGH)
-      digitalWrite(state4, HIGH)
-      digitalWrite(state3, HIGH)
-      digitalWrite(state2, HIGH)
-      digitalWrite(state1, HIGH)
-      digitalWrite(state6, LOW)
-      digitalWrite(state6, LOW)
-      digitalWrite(state5, LOW)
-      digitalWrite(state4, LOW)
-      digitalWrite(state3, LOW)
-      digitalWrite(state2, LOW)
-      digitalWrite(state1, LOW)
-      
-      digitalWrite(masterResetPin,LOW);
+    Serial.println("newNote");
+    for (int j=0; j<6; j++){
+      if (pins[j] != 0){
+        digitalWrite(pins[j],HIGH);
+        digitalWrite(pins[j],LOW);
+       // Serial.println(pins[j]);
+     }    
     }
+      digitalWrite(masterResetPin,LOW);
+      pins[0]= 0;
+      pins[1]= 0;
+      pins[2]= 0;
+      pins[3]= 0;
+      pins[4]= 0;
+      pins[5]= 0;
 
-unsigned int pushdownfret(String x, unsigned int out){
+  }
+   
+unsigned int pushdownfret(char x, unsigned int out){
 
 unsigned int dataout; 
-  if(x == "."){
+  if(x == '.'){
     dataout = out;
-}
-        
-  else if(x == "-"){
+  }    
+  else if(x == '-'){
        dataout = B00000;  
   }   
-  else if(x == "1"){
+  else if(x == '1'){
       dataout = B00001;  
   }
-  else if(x == "2"){
+  else if(x == '2'){
       dataout = B00010;  
   }
-  else if(x == "3"){
+  else if(x == '3'){
       dataout = B00100;     
   }
-  else if(x == "4"){
+  else if(x == '4'){
       dataout = B01000;
    }
-  else if(x == "5"){
+  else if(x == '5'){
       dataout = B10000;  
   }
-
-  else if(x == "0"){
- 
+  else if(x == '0'){
       dataout = B00000;
   }
-
   return dataout;
 }
 
 
-unsigned int strum(String x, unsigned int states, int right, int left){
-  unsigned int stateout;
-  if (x=="0" || x=="1" || x=="2" || x=="3" || x=="4" || x=="5"){
-    if (states == right){
-      stateout= left;
+void strum(char x, int string, int in, int out){
+  if (x=='0' || x=='1' || x=='2' || x=='3' || x=='4' || x=='5'){
+    if (actuators[string] == 1){
+      pins[string] = in;
+      actuators[string] = 0;
     }
     else{
-      stateout = right;
+      pins[string] = out;
+      actuators[string] = 1;
     }
   }
-  else{
-    stateout = states;
-  }
-   return stateout;
+
 }
